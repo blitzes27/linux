09a95158253254a9f26a07d15fdd1e7c310b21e2
Meron Tedros <meron.tedros@gmail.com>
2025-04-01 14:00:10 +0200

    Added my lazy NFS script nfs

---
+++ Random stuff/nfs_share.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# This script mounts an NFS share on Ubuntu server
+set -e
+# IP address of the NFS server
+nfs_ip="192.168.20.5"
+# Path to the NFS share on the server
+nfs_path="/volume7/data"
+# Mount point on the local machine
+nfs_mount_point="/mnt/data"
+# install nfs-common package
+apt update
+apt install nfs-common -y
+# Create the mount point directory if it doesn't exist
+mkdir -p /mnt/data
+# Mount the NFS share
+echo "$nfs_ip:$nfs_path $nfs_mount_point nfs defaults 0 0" | tee -a /etc/fstab > /dev/null
+# reload systemd
+systemctl daemon-reload
+# mount the NFS share
+mount -a
+# veriify the mount
+ls $nfs_mount_point

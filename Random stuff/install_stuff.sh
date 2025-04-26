6ec1df0873bf6462fabc8a1631aff3ae32ea13aa
Meron Tedros <meron.tedros@gmail.com>
2025-04-26 08:38:26 +0200

    comment

--- Random stuff/install_stuff.sh
+++ Random stuff/install_stuff.sh
@@ -3,10 +3,10 @@
 
 apt install tree -y
 apt install curl wget -y
-apt install net-tools iproute2 -y
+#apt install net-tools iproute2 -y
 apt install fail2ban -y
-apt install vim -y
-apt install nano -y
+#apt install vim -y
+#apt install nano -y
 apt install unzip -y
 apt install zip -y
 apt install rsync -y
